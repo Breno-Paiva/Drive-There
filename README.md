8d7002d9cfa28b338530239ddd9bc3808df847da
Breno Paiva <breno.paiva93@gmail.com>
2017-08-08 12:28:23 -0400

    clean code

--- README.md
+++ README.md
@@ -8,23 +8,17 @@
 # ruby
 @query = ActiveRecord::Base.connection.exec_query(<<-SQL)
   SELECT *
-  FROM vehicles
-  WHERE
-    vehicles.id NOT IN (
-      SELECT vehicles.id
-      FROM vehicles
-      JOIN reservations ON vehicles.id = reservations.vehicle_id
-      WHERE
-      (reservations.start_date > '#{request_start_date}' AND reservations.start_date < '#{request_end_date}') OR
-      (reservations.end_date > '#{request_start_date}' And reservations.end_date < '#{request_end_date}')
-    )
+    FROM vehicles
+   WHERE vehicles.id NOT IN
+         (SELECT vehicles.id
+            FROM vehicles
+            JOIN reservations ON vehicles.id = reservations.vehicle_id
+           WHERE
+                 (reservations.start_date > '#{request_start_date}'
+                 AND reservations.start_date < '#{request_end_date}')
+              OR (reservations.end_date > '#{request_start_date}'
+                 And reservations.end_date < '#{request_end_date}'))
 SQL
 ```
 
 ## Algorithm
-
-
-- list 1
-- list 2
-- list 3
-- list 4
--- drive_there/app/controllers/vehicles_controller.rb
+++ drive_there/app/controllers/vehicles_controller.rb
@@ -6,6 +6,7 @@ class VehiclesController < ApplicationController
       FROM
         vehicles
     SQL
+
     @reservations = ActiveRecord::Base.connection.exec_query(<<-SQL)
       SELECT
         *
@@ -18,32 +19,27 @@ class VehiclesController < ApplicationController
 
     @sub_query = ActiveRecord::Base.connection.exec_query(<<-SQL)
       SELECT *
-      FROM vehicles
-      JOIN reservations ON vehicles.id = reservations.vehicle_id
-      WHERE
-        (reservations.start_date > '#{request_start_date}' AND reservations.start_date < '#{request_end_date}') OR
-        (reservations.end_date > '#{request_start_date}' And reservations.end_date < '#{request_end_date}')
+         FROM vehicles
+         JOIN reservations ON vehicles.id = reservations.vehicle_id
+        WHERE
+              (reservations.start_date > '#{request_start_date}'
+              AND reservations.start_date < '#{request_end_date}')
+           OR (reservations.end_date > '#{request_start_date}'
+              And reservations.end_date < '#{request_end_date}')
     SQL
 
     @query = ActiveRecord::Base.connection.exec_query(<<-SQL)
       SELECT *
-      FROM vehicles
-      WHERE
-        vehicles.id NOT IN (
-          SELECT vehicles.id
-          FROM vehicles
-          JOIN reservations ON vehicles.id = reservations.vehicle_id
-          WHERE
-          (reservations.start_date > '#{request_start_date}' AND reservations.start_date < '#{request_end_date}') OR
-          (reservations.end_date > '#{request_start_date}' And reservations.end_date < '#{request_end_date}')
-        )
+        FROM vehicles
+       WHERE vehicles.id NOT IN
+             (SELECT vehicles.id
+                FROM vehicles
+                JOIN reservations ON vehicles.id = reservations.vehicle_id
+               WHERE
+                     (reservations.start_date > '#{request_start_date}'
+                     AND reservations.start_date < '#{request_end_date}')
+                  OR (reservations.end_date > '#{request_start_date}'
+                     And reservations.end_date < '#{request_end_date}'))
     SQL
   end
 end
-
-
-
-
-# WHERE
-# reservations.start_date < #{start_d} AND reservations.end_date < #{start_d} AND
-# reservations.start_date > #{end_d} And reservations.end_date > #{end_d}
